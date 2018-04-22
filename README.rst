8b58f936ca26b9cd8739156cd6100aa1f8338fe3
Sweyn Venderbush <sweyn3@yahoo.com>
2018-04-21 23:17:10 -0400

    Add readme changes

--- README.rst
+++ README.rst
@@ -1,7 +1,7 @@
 ==============================================================================
-UnFold Django Plugin.  UnFold plugin to support payments in Django backends
+UnFold Django Plugin
 ==============================================================================
-:Info: This is the README file for UnFold Plugin Django.
+:Info: UnFold plugin to support payments in Django backends.
 :Author: Sweyn Venderbush <sweyn.venderbush@yale.edu>
 :Copyright: © 2018, Sweyn Venderbush.
 :Date: 2018-05-01
@@ -21,6 +21,18 @@ To install, clone the UnFold plugin directory to your development environment. W
 
 You can also create a distributable Wheel of this module to include in distribution of your application. This module is not currently available via ``pip`` but is ready out of the box to be uploaded to `PyPi <https://pypi.python.org>`_.
 
+In your ``settings.py``, add ``unfold_plugin`` to Django REST framework's ``INSTALLED_APPS``::
+
+    INSTALLED_APPS = [
+        # ...
+        'unfold_plugin'
+    ]
+
+You also need to add your UnFold username and authentication key to your ``settings.py``::
+    
+    UNFOLD_AUTH_USERNAME = <your UnFold publisher username>
+    UNFOLD_AUTH_KEY = <your UnFold authentication key>
+
 USAGE
 -----
 The plugin consists of the decorator ``unfold_required`` which can be applied to any Django View. The decorator can be imported as follows and has the header::
@@ -36,13 +48,15 @@ The plugin consists of the decorator ``unfold_required`` which can be applied to
 It returns a dictionary of information about the article in the following format::
 
     {
-        'price': [the price you would like to charge for the article], 
-        'title': [the title of the article you would like displayed to the user],
-        'external_id': [the unique ID of the article that you would like to use to identify this article to UnFold]
+        'price': <the price you would like to charge for the article>, 
+        'title': <the title of the article you would like displayed to the user>,
+        'external_id': <the unique ID of the article that you would like to use to identify this article to UnFold>
     }
 
 If you're using class-based Views, the `unfold_required` can be applied to your URL as follows::
-
-    path('exampleview/', unfold_required(get_article_info)(ExampleView.as_view()))
+    urlpatterns = [
+        # ...
+        url('exampleview/', unfold_required(get_article_info)(ExampleView.as_view()))
+    ]
 
 For more information about using view decorators, see `View decorators <https://docs.djangoproject.com/en/2.0/topics/http/decorators/>`_ and `Class-based View decorators <https://docs.djangoproject.com/en/2.0/topics/class-based-views/intro/#decorating-class-based-views>`_.
--- unfold_plugin/unfold.py
+++ unfold_plugin/unfold.py
@@ -14,8 +14,11 @@ from django.shortcuts import redirect
 from django.conf import settings
 import requests
 
+
 _auth_key = settings.UNFOLD_AUTH_KEY
 _auth_username = settings.UNFOLD_AUTH_USERNAME
+
+HOST_URL = "http://localhost:8000"
 
 def unfold_required(get_article_attributes):
 
@@ -24,6 +27,7 @@ def unfold_required(get_article_attributes):
         def real_decorator(request, *args, **kwargs):
             attribs = get_article_attributes(request, *args, **kwargs)
             attribs['url'] = request.build_absolute_uri()
+            attribs['price'] = str(attribs['price'])
             register_article(_auth_key, attribs)
             username = request.session.get('unfold_user')
             token = request.GET.get('token')
@@ -32,7 +36,7 @@ def unfold_required(get_article_attributes):
                 if valid:
                     request.session['unfold_user'] = username
             if not username or not has_purchased(_auth_key, username, attribs['external_id']):
-                return redirect("http://localhost:8000/purchase?id=" + str(attribs['external_id']) + "&publisher=" + _auth_username)
+                return redirect(HOST_URL + "/purchase?id=" + str(attribs['external_id']) + "&publisher=" + _auth_username)
             else:
                 return f(request, *args, **kwargs)
 
@@ -43,23 +47,38 @@ def unfold_required(get_article_attributes):
 def validate_token(auth_key, token):
     headers ={'Authorization': 'JWT ' + auth_key}
     try:
-        r = requests.get("http://localhost:8000/api/validate-token?token=" + token, headers=headers)
+        r = requests.get(HOST_URL + "/api/validate-token", headers=headers, params={"token": token})
+        r.raise_for_status()
         return r.json()['valid'], r.json()['username']
-    except:
-        return False, None
+    except requests.exceptions.HTTPError as e:
+        if e.response.status_code == 401:
+            raise requests.exceptions.HTTPError('Authentication key is invalid')
+        else:
+            return False, None
 
 def has_purchased(auth_key, username, id):
     headers ={'Authorization': 'JWT ' + auth_key}
+    data = {
+        "username": username,
+        "id": id
+    }
     try:
-        r = requests.get("http://localhost:8000/api/has-access?username=" + username + "&id=" + str(id), headers=headers)
+        r = requests.get(HOST_URL + "/api/has-access", headers=headers, params=data)
+        r.raise_for_status()
         return r.json()['result']
-    except:
-        return False
+    except requests.exceptions.HTTPError as e:
+        if e.response.status_code == 401:
+            raise requests.exceptions.HTTPError('Authentication key is invalid')
+        else:
+            return False
 
 def register_article(auth_key, attribs):
     headers ={'Authorization': 'JWT ' + auth_key}
     try:
-        r = requests.post("http://localhost:8000/api/articles", headers=headers, data=attribs)
-    except:
-        pass
+        r = requests.post(HOST_URL + "/api/articles", headers=headers, data=attribs)
+        r.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        if e.response.status_code == 401:
+            raise requests.exceptions.HTTPError('Authentication key is invalid')
+        raise requests.exceptions.HTTPError('An error occurred while an article was trying to be registered')
 
